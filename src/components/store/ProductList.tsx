48e2715f939bb5f28d6bd8d801d60dc7608df350
Wix Remote Machine <support@wix.com>
2026-03-23 21:08:35 +0000

    Manual code changes

--- src/components/store/ProductList.tsx
+++ src/components/store/ProductList.tsx
@@ -52,7 +52,7 @@ interface ProductListProps {
 
 export const ProductListSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
+    <div className="">
       {Array.from({ length: 12 }).map((_, i) => (
         <Card
           key={i}
