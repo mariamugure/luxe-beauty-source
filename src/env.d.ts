d4271db11d44134b5b9e845a986f9a1509f3b886
Wix Remote Machine <support@wix.com>
2026-04-02 00:47:56 +0000

    Cannot publish website

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,10 @@
 import type { PageMetadata } from "@wix/astro-pages";
+import type { AstroGlobal } from "astro";
 
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
 
-declare const Astro: Readonly<import("astro").AstroGlobal>;
+declare const Astro: Readonly<AstroGlobal>;
 
 declare global {
   interface SDKTypeMode {
